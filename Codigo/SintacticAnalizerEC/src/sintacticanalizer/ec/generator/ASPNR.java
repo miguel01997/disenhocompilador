c1cf88ada04c31513407ba64622f2727ceaec7f8
alidainvernizzi <alidainvernizzi@ea2c166a-cd6a-11de-91bf-0503b81643b9>
2009-11-11 15:43:30 +0000

    ASPNR.java es la clase q realiza el analisis en si, MainASP es el disparador.. vendria a ser el principal del proyecto

---
+++ Codigo/SintacticAnalizerEC/src/sintacticanalizer/ec/generator/ASPNR.java
@@ -0,0 +1,111 @@
+package sintacticanalizer.ec.generator;
+
+import java.util.Vector;
+
+/*
+ * Analizador Sintactico Predictivo No Recursivo
+ */
+/**
+ *
+ * @author Alida
+ */
+public class ASPNR {
+
+    private int ip;
+    private String input;
+    private java.util.Stack stack;
+    private String M[][];
+    private Vector fila,  col;
+
+    public ASPNR(String[][] matriz, Vector fila, Vector col) {
+        stack = new java.util.Stack();
+        stack.push(fila.get(0).toString());
+        M = matriz;
+        this.fila = fila;
+        this.col = col;
+    }
+
+    /** Esta función realiza el analisis sintactico predictivo no recursivo
+     */
+    public void analizar(String input) {
+        String X;
+        StringBuffer P;
+        int r, c;
+        ip = 0;
+        this.input = input;
+        System.out.println("ASPNR analizando " + input);
+        System.out.println("S" + stack.toString() + ", I[" + input + "], P[]");
+        do {
+            X = ((String) stack.peek()).trim();
+            P = new StringBuffer();
+            if (findFila(X) == -1) { // X es terminal
+                if (X.equals(input)) {
+                    stack.pop();
+                    P.append("terminal: ");
+                    P.append(input);
+                    ip++;
+                    System.out.println("Stack\t" + stack.toString() + "\nInput\t" + input +
+                            "\nProd\t" + P.toString());
+                    break;
+                } else if (X.equals(col.get(0).toString()) && isID(input)) {
+                    stack.pop();
+                    P.append("id: ");
+                    P.append(input);
+                    ip++;
+                } else {
+                    System.out.println("ASPNR: unmatched terminal at position " + ip);
+                    break;
+                }
+            } else { // X es no-terminal
+                r = findFila(X);
+                c = findCol(input);
+                if (r != -1 && c != -1) {
+                    if (M[r][c] != null) {
+                        stack.pop();
+                        for (int s = M[r][c].length() - 2; s >= 0; s -= 2) {
+                            stack.push(M[r][c].substring(s, s + 2).trim());
+                        }
+                        P.append(X);
+                        P.append("->");
+                        P.append(M[r][c]);
+                    } else {
+                        System.out.println("ASPNR: no production");
+                        break;
+                    }
+                } else {
+                    System.out.println("ASPNR: unrecognized char at position " + ip);
+                    break;
+                }
+            }
+
+            System.out.println("Stack\t" + stack.toString() + "\nInput\t" + input +
+                    "\nProd\t" + P.toString());
+        } while (!stack.empty());
+
+    }
+
+    /** Verifica si el parametro es un no terminal, de ser asi retorna la
+     * posicion en el vector de no terminales, caso contrario retorna -1.
+     */
+    private int findFila(String nonTerminal) {
+
+        return (fila.indexOf(nonTerminal));
+
+    }
+
+    /** Verifica si el parametro es un terminal, de ser asi retorna la
+     * posicion en el vector de terminales, caso contrario retorna -1.
+     */
+    private int findCol(String terminal) {
+
+        return (col.indexOf(terminal));
+    }
+
+    private boolean isID(String id) {
+        if (id.charAt(0) >= 65 && id.charAt(0) <= 91 ||
+                id.charAt(0) >= 97 && id.charAt(0) <= 123) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Codigo/SintacticAnalizerEC/src/sintacticanalizer/ec/generator/MainASP.java
@@ -0,0 +1,155 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sintacticanalizer.ec.generator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.Vector;
+import sintacticanalizer.ec.components.PosicionMatrizProduccion;
+
+/**
+ *
+ * @author Alida
+ * Clase principal en la cual se llamaran a los metodos para generar la tabla ASP
+ * asi como al analizador sintactico.
+ */
+public class MainASP {
+
+    private GeneradorTablaASP generadorASP;
+    private List<PosicionMatrizProduccion> tabla;
+    private String matriz[][] = null;
+    private Vector fila1 = new Vector();
+    private Vector col1 = new Vector();
+    String cadena[] = {"id", "+", "id", "*", "id", "$"};
+
+    public MainASP() {
+
+        generadorASP = new GeneradorTablaASP();
+
+
+    }
+
+    /** Función que realiza la lectura de la gramatica insertada por el usuario
+     * y realiza la impresion de la misma en pantalla.
+     */
+    public List leerGramatica() throws IOException {
+        List gramaticas = new ArrayList();
+
+        String s;
+        System.out.println("Ingrese la gramatica: ");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        while ((s = in.readLine()).compareTo("null") != 0) {
+            gramaticas.add("" + s);
+        }
+        System.out.println("Gramatica a ser analizada: ");
+
+        for (int i = 0; i < gramaticas.size(); i++) {
+            String unaGramatica = (String) gramaticas.get(i);
+            StringTokenizer tokens = new StringTokenizer(unaGramatica, ">", false);
+            String leftPart = tokens.nextToken();
+            String rightPart = tokens.nextToken();
+
+            System.out.println(leftPart + "->" + rightPart);
+        }
+        return gramaticas;
+    }
+
+    /** Función que realiza la lectura de la cadena a ser comparada,
+     * insertada por el usuario y realiza la impresion de la misma en pantalla.
+     */
+    public void leerCadena() throws IOException {
+        Vector cad = new Vector();
+        String unaCadena = null;
+        String s;
+        System.out.println("Ingrese la gramatica: ");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        while ((s = in.readLine()).compareTo("null") != 0) {
+            cad.add("" + s);
+        }
+
+
+        for (int i = 0; i < cad.size(); i++) {
+            unaCadena = unaCadena + (String) cad.get(i);
+        }
+
+        System.out.println("Cadena a ser analizada: " + unaCadena);
+    //      this.cadena = cad;
+
+    }
+
+    /** Carga la matriz ASP la cual sera entrada para el ASPNR*/
+    public void cargarTabla() {
+        fila1 = new Vector();
+        col1 = new Vector();
+        int a, b;
+        matriz = new String[10][10];
+        tabla = generadorASP.getPosMatrizProdList();
+        for (int i = 0; i < tabla.size(); i++) {
+            PosicionMatrizProduccion pos = tabla.get(i);
+            String aux1 = pos.getNoTerminal();
+            String aux2 = pos.getTerminal();
+
+            if (!fila1.contains(aux1)) {
+                fila1.add(aux1);
+                a = fila1.indexOf(aux1);
+            } else {
+                a = fila1.indexOf(aux1);
+            }
+
+            if (!col1.contains(aux2)) {
+                col1.add(aux2);
+                b = col1.indexOf(aux2);
+            } else {
+                b = col1.indexOf(aux2);
+            }
+
+            matriz[a][b] = pos.getProduccion();
+        }
+        System.out.println(" Vector fila: ");
+        for (int j = 0; j < fila1.size(); j++) {
+            System.out.print(fila1.get(j) + "\t");
+        }
+        System.out.println();
+        System.out.println("\n Vector Columna: ");
+        for (int j = 0; j < col1.size(); j++) {
+            System.out.print(col1.get(j) + "\t");
+        }
+        System.out.println();
+        System.out.println(" \n Matriz ASP: ");
+        for (int k = 0; k < fila1.size(); k++) {
+            for (int j = 0; j < col1.size(); j++) {
+                System.out.print(matriz[k][j] + "\t");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+    /** Función que realiza la llamada a todos los metodos necesario para integrar
+     * el analizador, tanto de lectura de gramaticas y cadenas, asi como al generador
+     * de los conjuntos primero y sigte y de la tablaASP y por ultimo al ASPNR
+     */
+    public void principal() throws IOException {
+        // leerGramatica();
+
+        generadorASP.generar();
+        cargarTabla();
+        ASPNR asp = new ASPNR(matriz, fila1, col1);
+        for (int i = 0; i < cadena.length; i++) {
+            asp.analizar(cadena[i]);
+        }
+    }
+
+    public static void main(String argv[]) throws IOException {
+        new MainASP().principal();
+
+
+    }
+}
