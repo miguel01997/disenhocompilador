781a186112c1d967138efa9cb31204240a734af4
guiancs82 <guiancs82@gmail.com>
2009-11-17 21:54:31 +0000

    Analizador ASPNR

--- Codigo/SintacticAnalizerEC/src/sintacticanalizer/ec/generator/GeneradorASPNR.java
+++ Codigo/SintacticAnalizerEC/src/sintacticanalizer/ec/generator/GeneradorASPNR.java
@@ -32,7 +32,6 @@ public class GeneradorASPNR {
     private Vector fila1 = new Vector();
     private Vector col1 = new Vector();
     private String cadena[] = {"id", "+", "id", "*", "id", "$"};
-
     private List cadenaEntrada = null;
     private List<AnalisisASPNR> analisis = null;
 
@@ -87,11 +86,11 @@ public class GeneradorASPNR {
         }
 
         System.out.println("Cadena a ser analizada: " + unaCadena);
-    //      this.cadena = cad;
-
     }
 
-    /** Carga la matriz ASP la cual sera entrada para el ASPNR*/
+    /** 
+     * Carga la matriz ASP la cual sera entrada para el ASPNR
+     */
     public void cargarTabla() {
         fila1 = new Vector();
         col1 = new Vector();
@@ -179,32 +178,182 @@ public class GeneradorASPNR {
             for (int i = 0; i < cadena.length; i++) {
                 asp.analizar(cadena[i]);
             }
-        } else
+        } else {
             System.out.println("La gramatica es ambigua, esto se detecto en la tabla, " +
-                               "cuando dos producciones se iba a insertar en la misma posicion");
+                    "cuando dos producciones se iba a insertar en la misma posicion");
+        }
     }
 
-    /** Función que realiza la llamada a todos los metodos necesario para integrar
+    /**
+     * Cargar Terminales y No terminales.
+     */
+    private void cargarTerminalesYNoTerminales() {
+        fila1 = new Vector();
+        col1 = new Vector();
+        int a, b;
+        tabla = generadorASP.getPosMatrizProdList();
+
+        //1- Se obtiene todos los terminales y noterminales de la gramatica
+        for (int i = 0; i < tabla.size(); i++) {
+            PosicionMatrizProduccion pos = tabla.get(i);
+            String aux1 = pos.getNoTerminal();
+            String aux2 = pos.getTerminal();
+
+            if (!fila1.contains(aux1)) {
+                fila1.add(aux1);
+                a = fila1.indexOf(aux1);
+            } else {
+                a = fila1.indexOf(aux1);
+            }
+
+            if (!col1.contains(aux2)) {
+                col1.add(aux2);
+                b = col1.indexOf(aux2);
+            } else {
+                b = col1.indexOf(aux2);
+            }
+        }
+    }
+
+    /**
+     * Validar la cadena de entrada.
+     */
+    private boolean validarCadenaEntrada() {
+        boolean result = false;
+
+        int contadorValidos = 0;
+
+        for (int i = 0; i < cadenaEntrada.size(); i++) {
+
+            String unTerminalDeEntrada = (String) cadenaEntrada.get(i);
+
+            for (int j = 0; j < col1.size(); j++) {
+
+                String terminal = (String) col1.get(j);
+
+                if (terminal.compareTo(unTerminalDeEntrada) == 0) {
+                    result = true;
+                    contadorValidos++;
+                }
+
+            }
+
+            if (result == false) {
+                return result;
+            } else {
+                result = false;
+            }
+        }
+
+        if (contadorValidos == cadenaEntrada.size()) {
+            result = true;
+        } else {
+            result = false;
+        }
+
+        return result;
+    }
+
+    /** 
+     * Carga la matriz ASP la cual sera entrada para el ASPNR
+     */
+    public void cargarMatriz() {
+
+        int a, b;
+
+        //2- Se dimensiona la matriz y se carga
+        //en el contenido de la tabla en la matriz
+        matriz = new String[fila1.size()][col1.size()];
+        for (int i = 0; i < tabla.size(); i++) {
+            PosicionMatrizProduccion pos = tabla.get(i);
+            String aux1 = pos.getNoTerminal();
+            String aux2 = pos.getTerminal();
+
+            if (!fila1.contains(aux1)) {
+                fila1.add(aux1);
+                a = fila1.indexOf(aux1);
+            } else {
+                a = fila1.indexOf(aux1);
+            }
+
+            if (!col1.contains(aux2)) {
+                col1.add(aux2);
+                b = col1.indexOf(aux2);
+            } else {
+                b = col1.indexOf(aux2);
+            }
+
+            matriz[a][b] = pos.getProduccion();
+        }
+
+        System.out.println(" Vector fila: ");
+        for (int j = 0; j < fila1.size(); j++) {
+            System.out.print(fila1.get(j) + "\t");
+        }
+        System.out.println();
+        System.out.println("\n Vector Columna: ");
+        for (int j = 0; j < col1.size(); j++) {
+            System.out.print(col1.get(j) + "\t");
+        }
+        System.out.println();
+        System.out.println(" \n Matriz ASP: ");
+        for (int k = 0; k < fila1.size(); k++) {
+            for (int j = 0; j < col1.size(); j++) {
+                System.out.print(matriz[k][j] + "\t");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+    /**
+     * Función que realiza la llamada a todos los metodos necesario para integrar
      * el analizador, tanto de lectura de gramaticas y cadenas, asi como al generador
      * de los conjuntos primero y sigte y de la tablaASP y por ultimo al ASPNR
      */
     public RespuestaASPNR generar(List<Regla> reglas, RespuestaTablaASP estadoTablaASP) throws IOException {
 
         RespuestaASPNR estadoASPNR = new RespuestaASPNR();
-        
-        
+
+        //1- Validar si la tabla no tuvo error
         if (estadoTablaASP.getError() == false) {
-            cargarTabla();
-            ASPNR asp = new ASPNR(matriz, fila1, col1);
-            for (int i = 0; i < cadenaEntrada.size(); i++) {
-                asp.analizarTerminal(((String) cadenaEntrada.get(i)));
+
+            //2- Cargar los Terminales y No terminales
+            cargarTerminalesYNoTerminales();
+
+            //3- Verificar la valides de la cadena de Entrada
+            boolean esValida = validarCadenaEntrada();
+
+            if (esValida == true) {
+
+                //4- Cargar la Matriz
+                cargarMatriz();
+
+                //5- Analizar cada terminal de la cadena de entrada
+                ASPNR asp = new ASPNR(matriz, fila1, col1);
+                for (int i = 0; i < cadenaEntrada.size(); i++) {
+                    asp.analizarTerminal(((String) cadenaEntrada.get(i)));
+                }
+                analisis = asp.getAnalisis();
+
+                //6- Realizar la Derivación por la Izquierda.
+//                asp.derivarPorIzquierda();
+
+
+                estadoASPNR.setError(false);
+                estadoASPNR.setMensaje("Análisis realizada satisfactoriamente!!!");
+
+            } else {
+
+                estadoASPNR.setError(true);
+                estadoASPNR.setMensaje("La cadena de entrada es inválida!!!");
+
             }
 
-            analisis = asp.getAnalisis();
-            estadoASPNR.setError(false);
-            estadoASPNR.setMensaje("Análisis realizada satisfactoriamente!!!");
 
         } else {
+            
             estadoASPNR.setError(true);
             estadoASPNR.setMensaje(estadoTablaASP.getMensaje());
 
@@ -253,6 +402,4 @@ public class GeneradorASPNR {
     public void setAnalisis(List<AnalisisASPNR> analisis) {
         this.analisis = analisis;
     }
-
-    
 }
---
+++ Codigo/SintacticAnalizerEC/src/sintacticanalizer/ec/response/RespuestaPrimero.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sintacticanalizer.ec.response;
+
+/**
+ *
+ * @author Guido Casco
+ */
+public class RespuestaPrimero {
+
+    private boolean error = false;
+    private String mensaje = "";
+
+    public RespuestaPrimero(){
+    }
+
+    public boolean getError() {
+        return error;
+    }
+
+    public void setError(boolean error) {
+        this.error = error;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+
+
+}
